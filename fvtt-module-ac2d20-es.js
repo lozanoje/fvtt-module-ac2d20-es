d2142c519df40fa68e3beec5281d08a382c4c06c
José E. Lozano <lozanoje@gmail.com>
2024-01-15 18:23:58 +0100

    v11.2.0

--- fvtt-module-ac2d20-es.js
+++ fvtt-module-ac2d20-es.js
@@ -4,7 +4,32 @@ Hooks.once('init', () => {
 
 Hooks.once('ready', () => {
   if (game.user.isGM) {
-     console.log(game.settings.get("ac2d20", "compendium-skills"));
-      game.settings.set("ac2d20", "compendium-skills", 'fvtt-module-ac2d20-es.habilidades-es');
+    // console.log(game.settings.get("ac2d20", "compendium-skills"));
+    game.settings.set("ac2d20", "compendium-skills", 'fvtt-module-ac2d20-es.habilidades-es');
+	// fonts
+/*     game.settings.set("core", "fonts", {
+      "AC Cthulu Icons": {
+        "editor": true,
+        "fonts": [{
+            "urls": [
+              "systems/ac2d20/assets/fonts/AC-Cthulhu-Icons.woff"
+            ],
+            "weight": 400,
+            "style": "normal"
+          }
+        ]
+      },
+      "Cthulu Icons": {
+        "editor": true,
+        "fonts": [{
+            "urls": [
+              "systems/ac2d20/assets/fonts/cthulhu-icons.woff"
+            ],
+            "weight": 400,
+            "style": "normal"
+          }
+        ]
+      }
+    }) */
   }
 });
