ea652b1d079b83963310843f4f3f4f9e07fb42de
José Lozano <lozanoje@gmail.com>
2024-06-01 08:33:19 +0200

    Removed hovers-es.js

--- fvtt-module-ac2d20-es.js
+++ fvtt-module-ac2d20-es.js
@@ -5,7 +5,7 @@ Hooks.once('init', () => {
 Hooks.once('ready', () => {
   if (game.user.isGM) {
     // console.log(game.settings.get("ac2d20", "hoversJsonLocation"));
-    game.settings.set("ac2d20", "hoversJsonLocation", 'modules/fvtt-module-ac2d20-es/assets/hovers-es.json');
+    // game.settings.set("ac2d20", "hoversJsonLocation", 'modules/fvtt-module-ac2d20-es/assets/hovers-es.json');
     // console.log(game.settings.get("ac2d20", "compendium-skills"));
     // game.settings.set("ac2d20", "compendium-skills", 'fvtt-module-ac2d20-es.habilidades');
     // console.log(game.settings.get("ac2d20", "compendium-skills"));
